8266fe19d95788e6cb8917331b15fc670feed1d1
Inma Gijón <igijoninf@gmail.com>
2022-10-15 20:35:49 +0200

    Comprensión de listas

--- 3_Estructuras_Control_Random_Compresion/Comprension_Listas.py
+++ 3_Estructuras_Control_Random_Compresion/Comprension_Listas.py
@@ -5,3 +5,19 @@ lista = [letra for letra in palabra]
 #    lista.append(letra)
 
 print(lista)
+
+lista = [n/2 for n in range(0, 21, 2)]
+print(lista)
+
+lista = [n for n in range(0, 21, 2) if n * 2 > 10]
+print(lista)
+
+lista = [n if n * 2 > 10 else 'no' for n in range(0, 21, 2)]
+print(lista)
+
+# El coste de esto es la legibilidad pero es muy eficiente
+
+pulgadas = [10, 20, 30, 40, 50]
+centimetros = [p*2.54 for p in pulgadas]
+print(f'Pulgadas: {pulgadas}')
+print(f'Centimetros: {centimetros}')
