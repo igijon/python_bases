6dfb61f74d86b1932582f0efbb4033b5b62d42b2
Inma Gijón <igijoninf@gmail.com>
2022-10-15 20:01:59 +0200

    Range

---
+++ 3_Estructuras_Control_Random_Compresion/Range.py
@@ -0,0 +1,12 @@
+lista = [1, 2, 3, 4]
+for numero in lista:
+    print(numero)
+
+for numero in range(1, 5):  # El rango irá desde 1 hasta 4
+    print(numero)
+
+for numero in range(20, 31, 5):
+    print(numero)
+
+lista = list(range(1, 101))  # Crearía una lista del 1 al 100
+print(lista)
