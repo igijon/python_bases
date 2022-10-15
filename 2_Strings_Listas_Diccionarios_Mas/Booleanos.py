526f1fa5fc1b9a77f70cefa2453ffdd36e93095d
Inma Gijón <igijoninf@gmail.com>
2022-10-15 18:51:43 +0200

    Booleans

---
+++ 2_Strings_Listas_Diccionarios_Mas/Booleanos.py
@@ -0,0 +1,21 @@
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
+
+numero = 5 > 2+3
+print(numero)
+print(type(numero))
+
+numero = 5 != 2+3
+print(numero)
+
+numero = bool(5 > 2)
+print(numero)
+
+lista = [1, 2, 3, 4]
+control = 4 in lista
+print(control)
+print(type(control))
+control = 5 not in lista
+print(control, type(control))
