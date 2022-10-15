15828873193dea91bed972026262cc0c4a665468
Inma Gijón <igijoninf@gmail.com>
2022-10-15 19:12:58 +0200

    Operadores de comparación

---
+++ 3_Estructuras_Control_Random_Compresion/Operadores_Comparacion.py
@@ -0,0 +1,25 @@
+mi_bool = 10 == 25
+print(mi_bool, type(mi_bool))
+mi_bool = 5+5 == 18-8
+print(mi_bool, type(mi_bool))
+
+mi_bool = 'blanco' == 'negro'
+print(mi_bool)
+
+mi_bool = 'blanco' == 'Blanco'.lower()
+print(mi_bool)
+
+mi_bool = '100' == 100
+print(mi_bool)
+
+mi_bool = 100 == 100.0
+print(mi_bool)
+
+mi_bool = 100 != 99
+print(mi_bool)
+
+mi_bool = 5 > 5
+print(mi_bool)
+
+mi_bool = 5 >= 5
+print(mi_bool)
