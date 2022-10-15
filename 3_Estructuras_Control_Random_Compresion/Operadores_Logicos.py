8170bbec27bd51ddd24d21ba5945da75f20042bf
Inma Gijón <igijoninf@gmail.com>
2022-10-15 19:20:29 +0200

    Operadores lógicos

---
+++ 3_Estructuras_Control_Random_Compresion/Operadores_Logicos.py
@@ -0,0 +1,15 @@
+mi_bool = 4 < 5 < 6
+print(mi_bool)
+
+mi_bool = 4 < 5 and 'perro' == 'perro'
+print(mi_bool)
+
+mi_bool = 10 == 10 or 5 == 3
+print(mi_bool)
+
+texto = "Esta frase es breve"
+mi_bool = ('frase' in texto) or ('python' in texto)
+print(mi_bool)
+
+mi_bool = not ('a' == 'a')
+print(mi_bool)
