090ac3fe99f8b3b0ad1ba198800341f7fba7ff9a
Inma Gijón <igijoninf@gmail.com>
2022-10-16 09:41:10 +0200

    Return

---
+++ 4_Metodos_Funciones/Return.py
@@ -0,0 +1,13 @@
+def multiplicar(num1, num2):
+    """
+    :param num1: número operando
+    :param num2: número operando
+    :return: multiplicación de num1 y num2
+    """
+    return num1*num2
+
+
+resultado = multiplicar(4.23, 6)
+print(resultado)
+
+
