8216a95373ede99733a0f830ca008e6a7fbaa22f
Inma Gijón <igijoninf@gmail.com>
2022-10-16 09:14:12 +0200

    Ayuda con métodos

---
+++ 4_Metodos_Funciones/Ayuda.py
@@ -0,0 +1,7 @@
+dic = {'clave1': 100, 'clave2': 500}
+item_completo = dic.popitem()
+print(item_completo, type(item_completo))
+print(dic)
+
+# Ctrl+click vamos a la clase que implementa el método
+# Con el cursor encima obtengo ayuda contextual de cada método...
