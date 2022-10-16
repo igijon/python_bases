630c54a119545b0897ff862e4f680bf6fa95201b
Inma Gijón <igijoninf@gmail.com>
2022-10-16 10:09:59 +0200

    Funciones-Ejemplo con lista de tuplas

--- 4_Metodos_Funciones/Funciones_Dinamicas.py
+++ 4_Metodos_Funciones/Funciones_Dinamicas.py
@@ -13,5 +13,23 @@ def check_3_cifras2(lista):
 resultado = check_3_cifras(345)
 print(resultado)
 
-resultado = check_3_cifras2([2,34,567,67,678])
+resultado = check_3_cifras2([2, 34, 567, 67, 678])
 print(resultado)
+
+precios_cafe = [('capuchino', 1.5), ('Expresso', 1.2), ('Moka', 1.9)]
+
+
+def cafe_mas_caro(lista_precios):
+    precio_mayor = 0
+    cafe_caro = ''
+
+    for c, p in lista_precios:
+        if p > precio_mayor:
+            precio_mayor = p
+            cafe_caro = c
+
+    return cafe_caro, precio_mayor
+
+
+cafe, precio = cafe_mas_caro(precios_cafe)
+print(f'El café más caro es {cafe} cuyo precio es {precio}')
