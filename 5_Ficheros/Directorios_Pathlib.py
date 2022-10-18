66220b0fe4d1baedebcbc5cd176b74656b518791
Inma Gijón <igijoninf@gmail.com>
2022-10-18 10:23:31 +0200

    Más ejemplos con Pathlib

--- 5_Ficheros/Directorios_Pathlib.py
+++ 5_Ficheros/Directorios_Pathlib.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from pathlib import Path, PureWindowsPath
 
 """
 Este modo no nos asegura que funcione en distintos SSOO
@@ -13,3 +13,20 @@ archivo = carpeta / 'otro_ejemplo.txt'
 mi_archivo = open(archivo)
 print(mi_archivo.read())
 mi_archivo.close()
+
+ruta = Path('../recursos/otro_ejemplo.txt')
+print(ruta.read_text())  # No hemos tenido que abrir ni cerrar nuestro fichero
+print(ruta.name)  # Nombre del fichero
+print(ruta.suffix)
+print(ruta.stem)
+
+ruta_windows = PureWindowsPath(ruta)
+print(ruta_windows)
+
+if not ruta.exists():
+    print('Este archivo no existe')
+else:
+    print('Genial! Existe')
+
+
+
