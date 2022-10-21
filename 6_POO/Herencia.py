33fe01cf9815ea18be47928b5e64ad337d4d0502
Inma Gijón <igijoninf@gmail.com>
2022-10-21 13:23:01 +0200

    Herencia

--- 6_POO/Herencia.py
+++ 6_POO/Herencia.py
@@ -7,15 +7,30 @@ class Animal:
     def nacer(self):
         print('El animal ha nacido')
 
+    def hablar(self):
+        print('El animal emite un sonido')
+
 
 class Pajaro(Animal):
-    pass
+
+    def __init__(self, edad, color, altura_vuelo):
+        super(Pajaro, self).__init__(edad, color)
+        self.altura_vuelo = altura_vuelo
+
+    def hablar(self):
+        print('pio')
+
+    def volar(self, metros):
+        print(f'El pájaro vuela {metros} metros')
 
 
 print(Animal.__subclasses__())  # Me indica las clases que heredan de él
 print(Pajaro.__bases__)  # Me indica el padre
 
-piolin = Pajaro(2, 'verde')
-piolin.nacer()  # Lo ha heredado
-print(piolin.color)
+mi_animal = Animal(3, 'azul')
+piolin = Pajaro(2, 'verde', 10)
+piolin.hablar()
+piolin.volar(100)
+
+
 
