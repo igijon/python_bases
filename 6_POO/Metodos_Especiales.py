bfd0716e6d16e0ffe9fb14f44f30c34c382f0466
Inma Gijón <igijoninf@gmail.com>
2022-10-25 19:57:34 +0200

    Métodos especiales

---
+++ 6_POO/Metodos_Especiales.py
@@ -0,0 +1,37 @@
+mi_lista = [2, 2, 2, 2, 2, 2, 2]
+print(len(mi_lista))
+print(mi_lista)
+
+
+class Objeto:
+    pass
+
+
+mi_objeto = Objeto()
+# print(len(mi_objeto)) Dará error
+print(mi_objeto)  # Representación en un string de mi objeto básico
+
+
+class Album:
+    def __init__(self, autor, titulo, canciones):
+        self.autor = autor
+        self.titulo = titulo
+        self.canciones = canciones
+
+    def __str__(self):
+        return f'Album: {self.titulo} de {self.autor}'
+
+    def __len__(self):
+        return self.canciones
+
+    def __del__(self):
+        # Siempre lo va a borrar pero además hemos implementado que informe
+        print(f'Se ha borrado la instancia de album')
+
+mi_album = Album('Robe Iniesta', 'Bienvenidos al temporal', 19)
+print(mi_album)
+print(len(mi_album))
+
+del mi_album
+
+print(mi_album)  # Da error porque he borrado la instancia
