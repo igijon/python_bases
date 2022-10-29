36796479bfc097e16c93cfb5e3463dce6510478d
Inma Gijón <igijoninf@gmail.com>
2022-10-29 09:43:30 +0200

    Crear paquetes

---
+++ 8_Modulos_Paquetes/Paquete_Operaciones/suma_resta.py
@@ -0,0 +1,6 @@
+def suma(num1, num2):
+    print(num1+num2)
+
+def resta(num1, num2):
+    print(num1-num2)
+
---
+++ 8_Modulos_Paquetes/ejercicio.py
@@ -0,0 +1,5 @@
+from Paquete_Operaciones import suma_resta
+from Paquete_Operaciones.Subpaquetes import saludo
+
+suma_resta.resta(3, 4)
+saludo.hola()
