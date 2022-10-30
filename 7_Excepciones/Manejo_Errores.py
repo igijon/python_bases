294e6872da5afa7f7ec06fd822312314a6a045f1
Inma Gijón <igijoninf@gmail.com>
2022-10-30 09:01:54 +0100

    Tipos errores

--- 7_Excepciones/Manejo_Errores.py
+++ 7_Excepciones/Manejo_Errores.py
@@ -2,12 +2,15 @@ def suma():
     n1 = int(input('Número 1: '))
     n2 = int(input('Número 2: '))
     print(n1+n2)
-    print('Gracias por sumar')
+    print('Gracias por sumar '+n1+' y '+n2)
+
 
 try:
     suma()
-except:
-    print('Algo no ha salido bien')
+except TypeError:
+    print('Ha ocurrido un error de tipo')
+except ValueError:
+    print('Se ha introducido un carácter no numérico')
 else:
     print('Hiciste todo bien')
 finally:
